68c565f71bc6e741c845b8597d3f3693ead119fb
johnlin1066 <johnlin1066@gmail.com>
2023-07-29 18:28:27 +0800

    upgrade to 1.0.0
    upgrade to 1.0.0

--- src/main/java/com/example/fugle_realtime_java_sdk_example_springboot_resttemplate/tool/HttpToolImp.java
+++ src/main/java/com/example/fugle_realtime_java_sdk_example_springboot_resttemplate/tool/HttpToolImp.java
@@ -1,21 +1,117 @@
 package com.example.fugle_realtime_java_sdk_example_springboot_resttemplate.tool;
 
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpEntity;
+import org.springframework.util.MultiValueMap;
+import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
-import com.example.fugle_realtime_java_sdk_core.dto.response.ResponseObject;
+import com.example.fugle_realtime_java_sdk_core.historical.response.HistoricalCandlesResponse;
+import com.example.fugle_realtime_java_sdk_core.historical.response.StatsResponse;
+import com.example.fugle_realtime_java_sdk_core.intraday.response.CandlesResponse;
+import com.example.fugle_realtime_java_sdk_core.intraday.response.QuoteResponse;
+import com.example.fugle_realtime_java_sdk_core.intraday.response.TickerResponse;
+import com.example.fugle_realtime_java_sdk_core.intraday.response.TickersResponse;
+import com.example.fugle_realtime_java_sdk_core.intraday.response.TradesResponse;
+import com.example.fugle_realtime_java_sdk_core.intraday.response.VolumesResponse;
+import com.example.fugle_realtime_java_sdk_core.snapshot.response.ActivesResponse;
+import com.example.fugle_realtime_java_sdk_core.snapshot.response.MoversResponse;
+import com.example.fugle_realtime_java_sdk_core.snapshot.response.QuotesResponse;
+import com.example.fugle_realtime_java_sdk_core.tool.Constant;
 import com.example.fugle_realtime_java_sdk_core.tool.HttpTool;
 
 public class HttpToolImp implements HttpTool{
 	
 	RestTemplate restTemplate;
+	Constant constant;
 	
-	public HttpToolImp(RestTemplate restTemplate) {
+	public HttpToolImp(RestTemplate restTemplate, Constant constant) {
 		this.restTemplate = restTemplate;
+		this.constant = constant;
 	}
 
 	@Override
-	public ResponseObject getForObject(String url) {
-		return restTemplate.getForObject(url, ResponseObject.class);
+	public TickersResponse getForTickersResponse(String url) {
+		ResponseEntity<TickersResponse> response = restTemplate.exchange(url, HttpMethod.GET, addHeader(),
+				TickersResponse.class);
+		return response.getBody();
+	}
+	HttpEntity<Object> addHeader(){
+		MultiValueMap<String, String> headers = new LinkedMultiValueMap<String, String>();
+		headers.add("X-API-KEY", constant.getxApiKey());
+		HttpEntity<Object> requestEntity = new HttpEntity<Object>(headers);
+		return requestEntity;
+	}
+	
+	@Override
+	public TickerResponse getForTickerResponse(String url) {
+		ResponseEntity<TickerResponse> response = restTemplate.exchange(url, HttpMethod.GET, addHeader(),
+				TickerResponse.class);
+		return response.getBody();
+	}
+
+	@Override
+	public QuoteResponse getForQuoteResponse(String url) {
+		ResponseEntity<QuoteResponse> response = restTemplate.exchange(url, HttpMethod.GET, addHeader(),
+				QuoteResponse.class);
+		return response.getBody();
+	}
+
+	@Override
+	public CandlesResponse getForCandlesResponse(String url) {
+		ResponseEntity<CandlesResponse> response = restTemplate.exchange(url, HttpMethod.GET, addHeader(),
+				CandlesResponse.class);
+		return response.getBody();
+	}
+
+	@Override
+	public TradesResponse getForTradesResponse(String url) {
+		ResponseEntity<TradesResponse> response = restTemplate.exchange(url, HttpMethod.GET, addHeader(),
+				TradesResponse.class);
+		return response.getBody();
+	}
+
+	@Override
+	public VolumesResponse getForVolumesResponse(String url) {
+		ResponseEntity<VolumesResponse> response = restTemplate.exchange(url, HttpMethod.GET, addHeader(),
+				VolumesResponse.class);
+		return response.getBody();
+	}
+
+	@Override
+	public QuotesResponse getForQuotesResponse(String url) {
+		ResponseEntity<QuotesResponse> response = restTemplate.exchange(url, HttpMethod.GET, addHeader(),
+				QuotesResponse.class);
+		return response.getBody();
+	}
+
+	@Override
+	public MoversResponse getForMoversResponse(String url) {
+		ResponseEntity<MoversResponse> response = restTemplate.exchange(url, HttpMethod.GET, addHeader(),
+				MoversResponse.class);
+		return response.getBody();
+	}
+
+	@Override
+	public ActivesResponse getForActivesResponse(String url) {
+		ResponseEntity<ActivesResponse> response = restTemplate.exchange(url, HttpMethod.GET, addHeader(),
+				ActivesResponse.class);
+		return response.getBody();
+	}
+
+	@Override
+	public HistoricalCandlesResponse getForHistoricalCandlesResponse(String url) {
+		ResponseEntity<HistoricalCandlesResponse> response = restTemplate.exchange(url, HttpMethod.GET, addHeader(),
+				HistoricalCandlesResponse.class);
+		return response.getBody();
+	}
+
+	@Override
+	public StatsResponse getForStatsResponse(String url) {
+		ResponseEntity<StatsResponse> response = restTemplate.exchange(url, HttpMethod.GET, addHeader(),
+				StatsResponse.class);
+		return response.getBody();
 	}
 
 }
--- src/test/java/com/example/fugle_realtime_java_sdk_example_springboot_resttemplate/tool/HttpToolImpTest.java
+++ src/test/java/com/example/fugle_realtime_java_sdk_example_springboot_resttemplate/tool/HttpToolImpTest.java
@@ -2,11 +2,22 @@ package com.example.fugle_realtime_java_sdk_example_springboot_resttemplate.tool
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import com.example.fugle_realtime_java_sdk_core.dto.response.ResponseObject;
+import com.example.fugle_realtime_java_sdk_core.historical.response.HistoricalCandlesResponse;
+import com.example.fugle_realtime_java_sdk_core.historical.response.StatsResponse;
+import com.example.fugle_realtime_java_sdk_core.intraday.response.CandlesResponse;
+import com.example.fugle_realtime_java_sdk_core.intraday.response.QuoteResponse;
+import com.example.fugle_realtime_java_sdk_core.intraday.response.TickerResponse;
+import com.example.fugle_realtime_java_sdk_core.intraday.response.TickersResponse;
+import com.example.fugle_realtime_java_sdk_core.intraday.response.TradesResponse;
+import com.example.fugle_realtime_java_sdk_core.intraday.response.VolumesResponse;
+import com.example.fugle_realtime_java_sdk_core.snapshot.response.ActivesResponse;
+import com.example.fugle_realtime_java_sdk_core.snapshot.response.MoversResponse;
+import com.example.fugle_realtime_java_sdk_core.snapshot.response.QuotesResponse;
 import com.example.fugle_realtime_java_sdk_core.tool.HttpTool;
 
 @SpringBootTest
@@ -16,11 +27,113 @@ public class HttpToolImpTest {
 	HttpTool httpTool;
 	
 	@Test
-	void getForObject() {
-		String url = "https://api.fugle.tw/realtime/v0.3/intraday/chart?symbolId=2884&apiToken=demo";
+	@Disabled("test")
+	public void getForTickersResponse() {
+		String url = "https://api.fugle.tw/marketdata/v1.0/stock/intraday/tickers?type=EQUITY&exchange=TWSE"
+				+ "&market=TSE&industry=01&isNormal=true";
 		
-		ResponseObject responseObject =  httpTool.getForObject(url);
+		TickersResponse responseObject = httpTool.getForTickersResponse(url);
 		
+		assertEquals(true, null!=responseObject);
+	}
+	
+	@Test
+	@Disabled("test")
+	public void getForTickerResponse() {
+		String url = "https://api.fugle.tw/marketdata/v1.0/stock/intraday/ticker/2330";
+		
+		TickerResponse responseObject = httpTool.getForTickerResponse(url);
+		
+		assertEquals(true, null!=responseObject);
+	}
+	
+	@Test
+	@Disabled("test")
+	public void getForQuoteResponse() {
+		String url = "https://api.fugle.tw/marketdata/v1.0/stock/intraday/quote/2330";
+		
+		QuoteResponse responseObject = httpTool.getForQuoteResponse(url);
+		
+		assertEquals(true, null!=responseObject);
+	}
+	
+	@Test
+	@Disabled("test")
+	public void getForCandlesResponse() {
+		String url = "https://api.fugle.tw/marketdata/v1.0/stock/intraday/candles/2330";
+		
+		CandlesResponse responseObject = httpTool.getForCandlesResponse(url);
+		
+		assertEquals(true, null!=responseObject);
+	}
+	
+	@Test
+	@Disabled("test")
+	public void getForTradesResponse() {
+		String url = "https://api.fugle.tw/marketdata/v1.0/stock/intraday/trades/0050";
+		
+		TradesResponse responseObject = httpTool.getForTradesResponse(url);
+		
+		assertEquals(true, null!=responseObject);
+	}
+	
+	@Test
+	@Disabled("test")
+	public void getForVolumesResponse() {
+		String url = "https://api.fugle.tw/marketdata/v1.0/stock/intraday/volumes/2330";
+		
+		VolumesResponse responseObject = httpTool.getForVolumesResponse(url);
+		
+		assertEquals(true, null!=responseObject);
+	}
+	
+	@Test
+	@Disabled("test")
+	public void getForQuotesResponse() {
+		String url = "https://api.fugle.tw/marketdata/v1.0/stock/snapshot/quotes/TSE";
+		
+		QuotesResponse responseObject = httpTool.getForQuotesResponse(url);
+
+		assertEquals(true, null!=responseObject);
+	}
+
+	@Test
+	@Disabled("test")
+	public void getForMoversResponse() {
+		String url = "https://api.fugle.tw/marketdata/v1.0/stock/snapshot/movers/TSE?direction=up&change=percent";
+		
+		MoversResponse responseObject = httpTool.getForMoversResponse(url);
+
+		assertEquals(true, null!=responseObject);
+	}
+
+	@Test
+	@Disabled("test")
+	public void getForActivesResponse() {
+		String url = "https://api.fugle.tw/marketdata/v1.0/stock/snapshot/actives/TSE?trade=value";
+		
+		ActivesResponse responseObject = httpTool.getForActivesResponse(url);
+
+		assertEquals(true, null!=responseObject);
+	}
+	
+	@Test
+	@Disabled("test")
+	public void getForHistoricalCandlesResponse() {
+		String url = "https://api.fugle.tw/marketdata/v1.0/stock/historical/candles/0050?fields=open,high,low,close,volume";
+		
+		HistoricalCandlesResponse responseObject = httpTool.getForHistoricalCandlesResponse(url);
+
+		assertEquals(true, null!=responseObject);
+	}
+
+	@Test
+	@Disabled("test")
+	public void getForStatsResponse() {
+		String url = "https://api.fugle.tw/marketdata/v1.0/stock/historical/stats/0050";
+		
+		StatsResponse responseObject = httpTool.getForStatsResponse(url);
+
 		assertEquals(true, null!=responseObject);
 	}
 }
